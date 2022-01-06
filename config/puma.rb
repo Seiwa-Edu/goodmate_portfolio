1aac9f90c81efd2c25f68767335e12d37fb1f0c8
Seiwa-Edu <kuma.0131@icloud.com>
2022-01-07 07:42:40 +0900

    アプリケーションサーバーの設定を修正

--- config/puma.rb
+++ config/puma.rb
@@ -8,6 +8,8 @@ max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
 
+bind "unix:///var/www/goodmate_portfolio/tmp/sockets/puma.sock"
+
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 #
@@ -15,7 +17,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
